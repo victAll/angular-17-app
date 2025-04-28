6224a62d7d7d38e9904a439709668b3b9067ca97
DESKTOP-6IKIV4K\victo <victormanuel.allende@gmail.com>
2025-04-28 12:14:31 +0200

    1 primer commit angular 17 tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-17-app';
+  city = 'Madrid';
 }
